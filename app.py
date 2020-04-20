69059e3d509150a263b2e89a77cf60ea9b50d390
Ihor N <ihnesto@gmail.com>
2020-04-20 09:29:10 +0300

    create index.html page

--- app.py
+++ app.py
@@ -1,13 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
+tasks = [
+    { "id" : 1, "name" : 'Watch TV'},
+    { "id" : 2, "name" : 'buy eggs'}
+   
+]
+
 @app.route('/')
 def index() :
-    return 'ok'
-
+    return render_template('index.html', tasks = tasks)
 
 if __name__ == '__main__' :
     app.run(debug=True)
-    
-    
+
